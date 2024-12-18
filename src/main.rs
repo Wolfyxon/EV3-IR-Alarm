c3e6c9ccbc3ff3edaf85b8a6710f3631089c73f3
Wolfyxon <wolfyxon@gmail.com>
2024-12-18 21:47:58 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use ev3dev_lang_rust::{sensors::InfraredSensor, sound, Ev3Result, Led};
+use std::{thread, time};
+
+fn main() -> Ev3Result<()> {
+    let ir = InfraredSensor::find().expect("IR sensor not connected");
+    let led = Led::new().expect("LED error");
+
+    let colors = vec![
+        Led::COLOR_YELLOW, 
+        Led::COLOR_AMBER,  
+        Led::COLOR_RED
+    ];
+
+    for i in 0..colors.len() {
+        println!("Arming in {}...", colors.len() - i);
+
+        led.set_color(colors[i])?;
+        sleep(1.0);
+    }
+
+    led.set_color(Led::COLOR_OFF)?;
+    
+    let threshold = 2;
+    let init_dist = ir.get_distance().expect("Failed to get distance");
+
+    println!("Alarm armed");
+    println!("Initial distance: {}", init_dist);
+
+    loop {
+        match ir.get_distance() {
+            Ok(dist) => {
+                if (dist - init_dist).abs() >= threshold {
+                    println!("MOTION DETECTED!");
+                    
+                    let mut switch = false;
+                    
+                    loop {
+                        switch = !switch;
+                        
+                        let mut freq = 1000.0;
+
+                        if switch {
+                            freq = 500.0;
+                            led.set_color(Led::COLOR_RED)?;
+                        } else {
+                            led.set_color(Led::COLOR_OFF)?;
+                        }
+
+                        sound::tone(freq, 50)?.wait()?;
+                    }
+                }
+            },
+
+            Err(err) => {
+                println!("Failed to get distance: {:?}", err);
+                continue;
+            }
+        }
+    }
+}
+
+fn sleep(s: f32) {
+    thread::sleep(time::Duration::from_secs_f32(s));
+}
